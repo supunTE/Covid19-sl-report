525a6b8d2b909588376e936936ebbacac2047d9a
STEdirisuriya <supunedirisuriya2002@gmail.com>
2020-05-01 16:08:48 +0530

    Added country selector with letters, improved the theme

--- js/script-global.js
+++ js/script-global.js
@@ -11,7 +11,7 @@ globalTotal = document.getElementById('global_total');
 globalDeaths = document.getElementById('global_deaths');
 globalRecovered = document.getElementById('global_recovered');
 globalActive = document.getElementById('global_active');
-globalCritical= document.getElementById('global_critical');
+globalCritical = document.getElementById('global_critical');
 globalTests = document.getElementById('global_tests');
 globalCasesToday = document.getElementById('global_cases_today');
 globalDeathsToday = document.getElementById('global_deaths_today');
@@ -46,63 +46,63 @@ LastUpdated = document.getElementById('API_lastUpdated');
 //   console.log(data);
 // }
 
-function getDatefromStamp(timeStamp){
-  var a = new Date(timeStamp); 
-  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+function getDatefromStamp(timeStamp) {
+  var a = new Date(timeStamp);
+  var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   var year = a.getUTCFullYear();
   var month = months[a.getMonth()];
   var date = a.getDate();
   var hour = a.getHours();
-  if(hour>12){
+  if (hour > 12) {
     var ap = "PM";
-  }else{
+  } else {
     var ap = "AM";
   }
-  if(hour>12){
+  if (hour > 12) {
     hour -= 12;
   }
   var min = a.getMinutes();
   var sec = a.getSeconds();
   var sec = a.getSeconds();
 
-  var time = date + ' ' + month + ' ' + year + ' | ' + hour + ':' + min + ':' + sec + ' ' + ap ;
+  var time = date + ' ' + month + ' ' + year + ' | ' + hour + ':' + min + ':' + sec + ' ' + ap;
   return time;
 }
 
-function getData(){
-fetch(api_url)
-  .then(function(response) {
-    if (response.status >= 400) {
-      throw new Error("Bad response from server");
-    }
-    return response.json();
-  })
+function getData() {
+  fetch(api_url)
+    .then(function (response) {
+      if (response.status >= 400) {
+        throw new Error("Bad response from server");
+      }
+      return response.json();
+    })
 
-  .then(function(json) {
-    globalTotalAPI = json.cases;
-    globalDeathsAPI = json.deaths;
-    globalRecoveredAPI = json.recovered;
-    globalActiveAPI = json.active;
-    globalCriticalAPI = json.critical;
-    globalTestsAPI = json.tests;
-    globalCasesTodayAPI = json.todayCases;
-    globalDeathsTodayAPI = json.todayDeaths;
-    // casesPerOneMillionAPI = json.casesPerOneMillion;
-    // deathsPerOneMillionAPI = json.deathsPerOneMillion;
-    // testsPerOneMillionAPI = json.testsPerOneMillion;
-    // affectedCountriesAPI = json.affectedCountries;
-    LastUpdatedAPI = getDatefromStamp(json.updated);
+    .then(function (json) {
+      globalTotalAPI = json.cases;
+      globalDeathsAPI = json.deaths;
+      globalRecoveredAPI = json.recovered;
+      globalActiveAPI = json.active;
+      globalCriticalAPI = json.critical;
+      globalTestsAPI = json.tests;
+      globalCasesTodayAPI = json.todayCases;
+      globalDeathsTodayAPI = json.todayDeaths;
+      // casesPerOneMillionAPI = json.casesPerOneMillion;
+      // deathsPerOneMillionAPI = json.deathsPerOneMillion;
+      // testsPerOneMillionAPI = json.testsPerOneMillion;
+      // affectedCountriesAPI = json.affectedCountries;
+      LastUpdatedAPI = getDatefromStamp(json.updated);
 
-    globalTotal.innerHTML = formatNumber(globalTotalAPI);
-    globalDeaths.innerHTML = formatNumber(globalDeathsAPI);
-    globalRecovered.innerHTML = formatNumber(globalRecoveredAPI);
-    globalActive.innerHTML = formatNumber(globalActiveAPI)
-    globalCritical.innerHTML = formatNumber(globalCriticalAPI)
-    globalTests.innerHTML = formatNumber(globalTestsAPI)
-    globalCasesToday.innerHTML = 'Today: +' + formatNumber(globalCasesTodayAPI);
-    globalDeathsToday.innerHTML = 'Today: +' + formatNumber(globalDeathsTodayAPI);
-    // LastUpdated.innerHTML = LastUpdatedAPI;
-  })
+      globalTotal.innerHTML = formatNumber(globalTotalAPI);
+      globalDeaths.innerHTML = formatNumber(globalDeathsAPI);
+      globalRecovered.innerHTML = formatNumber(globalRecoveredAPI);
+      globalActive.innerHTML = formatNumber(globalActiveAPI)
+      globalCritical.innerHTML = formatNumber(globalCriticalAPI)
+      globalTests.innerHTML = formatNumber(globalTestsAPI)
+      globalCasesToday.innerHTML = 'Today: +' + formatNumber(globalCasesTodayAPI);
+      globalDeathsToday.innerHTML = 'Today: +' + formatNumber(globalDeathsTodayAPI);
+      // LastUpdated.innerHTML = LastUpdatedAPI;
+    })
 }
 
 getData();
@@ -110,30 +110,30 @@ getData();
 // global news
 //  https://newsapi.org/docs
 var url = 'https://newsapi.org/v2/everything?' +
-'q=COVID+corona&' +
-'language=en&' +
-'pageSize=5&' +
-'excludeDomains=Youtube.com&' +
-'sortBy=relevancy&' +
-'apiKey=6814e8af1172455a95052c18d4a90f8b';
+  'q=COVID+corona&' +
+  'language=en&' +
+  'pageSize=5&' +
+  'excludeDomains=Youtube.com&' +
+  'sortBy=relevancy&' +
+  'apiKey=6814e8af1172455a95052c18d4a90f8b';
 
 var globalNews = document.getElementById('globalNews');
 
 fetch(url)
-.then(function (response) {
-  if (response.status >= 400) {
-    throw new Error("Bad response from server");
-  }
-  return response.json();
-})
+  .then(function (response) {
+    if (response.status >= 400) {
+      throw new Error("Bad response from server");
+    }
+    return response.json();
+  })
 
-.then(function (json) {
-  var length = json.articles.length;
-  for (a = 0; a < length; a++) {
-    t = json.articles[a]
-    globalNewsFunction(t.title, t.description, t.urlToImage, t.url, t.publishedAt, t.author, t.source.name);
-  }
-})
+  .then(function (json) {
+    var length = json.articles.length;
+    for (a = 0; a < length; a++) {
+      t = json.articles[a]
+      globalNewsFunction(t.title, t.description, t.urlToImage, t.url, t.publishedAt, t.author, t.source.name);
+    }
+  })
 
 function globalNewsFunction(title, desc, imgUrl, url, time, author, source) {
   let a = document.createElement('a');
@@ -161,9 +161,9 @@ function globalNewsFunction(title, desc, imgUrl, url, time, author, source) {
   timeHTML.setAttribute('id', 'global_new_time');
   timeHTML.textContent = time.substring(0, 10);;
   authorHTML.setAttribute('id', 'global_new_author');
-  if(author){
+  if (author) {
     authorHTML.textContent = author + '(' + source + ')';
-  }else{
+  } else {
     authorHTML.textContent = source;
   }
   bodyHTML.appendChild(titleHTML);
@@ -181,25 +181,33 @@ function globalNewsFunction(title, desc, imgUrl, url, time, author, source) {
 // countries details
 var url_countries = 'https://corona.lmao.ninja/v2/countries?sort=country';
 
-var countriesDetails = document.getElementById('countriesDetails');
+var countriesDetailsABC = document.getElementById('nav-abc');
+var countriesDetailsDEF = document.getElementById('nav-def');
+var countriesDetailsGHI = document.getElementById('nav-ghi');
+var countriesDetailsJKL = document.getElementById('nav-jkl');
+var countriesDetailsMNO = document.getElementById('nav-mno');
+var countriesDetailsPQRS = document.getElementById('nav-pqrs');
+var countriesDetailsTUV = document.getElementById('nav-tuv');
+var countriesDetailsWXYZ = document.getElementById('nav-wxyz');
 
 fetch(url_countries)
-.then(function (response) {
-  if (response.status >= 400) {
-    throw new Error("Bad response from server");
-  }
-  return response.json();
-})
+  .then(function (response) {
+    if (response.status >= 400) {
+      throw new Error("Bad response from server");
+    }
+    return response.json();
+  })
 
-.then(function (json) {
-  var length = json.length;
-  for (a = length-1; a > 0; a--) {
-    t = json[a];
-    countryDetailsFunction(t);
-  }
-})
+  .then(function (json) {
+    var length = json.length;
+    for (a = length - 1; a > 0; a--) {
+      t = json[a];
+      countryLetter = t.country.charAt(0);
+      countryDetailsFunction(t, countryLetter);
+    }
+  })
 
-function countryDetailsFunction(t) {
+function countryDetailsFunction(t, letters) {
   let cardHTML = document.createElement('div');
   let imgHTML = document.createElement('div');
   let img = document.createElement('img');
@@ -219,7 +227,7 @@ function countryDetailsFunction(t) {
   let dpmHTML = document.createElement('h6');
   let tpmHTML = document.createElement('h6');
   let hr = document.createElement('hr');
-  
+
   cardHTML.setAttribute('class', 'country-details-card bg-transparent-dark b-dark text-dark');
   imgHTML.setAttribute('class', 'col-2 country_flag_image');
   img.setAttribute('class', 'country_flag');
@@ -263,6 +271,48 @@ function countryDetailsFunction(t) {
   moreHTML.appendChild(tpmHTML);
   cardHTML.appendChild(moreHTML);
 
-  countriesDetails.appendChild(cardHTML);
-  
+  switch (letters) {
+    case "A":
+    case "B":
+    case "C":
+      countriesDetailsABC.appendChild(cardHTML);
+      break;
+    case "D":
+    case "E":
+    case "F":
+      countriesDetailsDEF.appendChild(cardHTML);
+      break;
+    case "G":
+    case "H":
+    case "I":
+      countriesDetailsGHI.appendChild(cardHTML);
+      break;
+    case "J":
+    case "K":
+    case "L":
+      countriesDetailsJKL.appendChild(cardHTML);
+      break;
+    case "M":
+    case "N":
+    case "O":
+      countriesDetailsMNO.appendChild(cardHTML);
+      break;
+    case "P":
+    case "Q":
+    case "R":
+    case "S":
+      countriesDetailsPQRS.appendChild(cardHTML);
+      break;
+    case "T":
+    case "U":
+    case "V":
+      countriesDetailsTUV.appendChild(cardHTML);
+      break;
+    case "W":
+    case "X":
+    case "Y":
+    case "Z":
+      countriesDetailsWXYZ.appendChild(cardHTML);
+      break;
+  }
 }
